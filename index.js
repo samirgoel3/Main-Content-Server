e3f3fbb2bc6300d53e3ef0364a53c8aadd96929e
Samir Goel <samirgoel3@gmail.com>
2022-09-21 15:20:00 +0530

    some initial Code is added

--- index.js
+++ index.js
@@ -1,13 +1,69 @@
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 3001 ;
+// const route = require('./routes')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const cors = require('cors');
+const { ServerApiVersion } = require('mongodb');
+const fileUpload = require('express-fileupload')
+const path = require('path')
+
+let serverApp =  express();
+serverApp.set('hostname', 'localhost');
+        serverApp.set('port', process.env.PORT || 3001);
+
+        serverApp.use(fileUpload())
+
+        // CORS
+        serverApp.options('*', cors());
+        serverApp.use(function (req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+            res.header("Access-Control-Allow-Headers", "Origin, x-access-token, Content-Type, Accept");
+            next();
+        });
+
+        // Middleware
+
+        serverApp.use(bodyParser.json({limit: '50mb', extended: true}));
+        serverApp.use(bodyParser.urlencoded({extended: false}));
 
 
 
-app.use('/', (req, res)=>{
-    res.send('Hello , I am Main Content Server, i ill save images in folder and provide link')
-})
 
-app.listen(port, ()=>{
-    console.log("*****---> App listening on port: "+port)
-})
+        mongoose.connect('mongodb+srv://MainClusture:94QikJPKmTndGaE3@mainclusture.l3f41ws.mongodb.net/?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverApi:ServerApiVersion.v1,
+            dbName:'Content-Database' ,
+            autoIndex: true,
+        })
+            .then((res) => {console.log('#####---> Mongo DB Connected!');})
+            .catch(err => {console.log("####----> Mongo Db not Connected" + err);});
+
+        serverApp.use('/images', express.static(path.join(__dirname, '../images')));
+
+
+        serverApp.use('*', (req, res, next) => {
+            console.log('Request was made to : ' + req.method + " -> " + req.originalUrl+ '\n*******************');
+            next();
+        });
+    
+    
+        serverApp.get('/', (req, res)=>{
+            res.send('Hi I am CONTENT SERVER, and i will save file and provide url :). ')
+        })
+
+
+        // starting server
+        serverApp.listen(serverApp.get("port"), () => {
+            console.log(
+                "Express Server is listening on - https://" + serverApp.get("hostname") + ":" + serverApp.get("port")
+            );
+        });
+
+
+
+
+
+
+
