61e6ddc2ec68ca259b2fb1ba07bec12958d9a0d7
Samir Goel <samirgoel3@gmail.com>
2022-09-21 15:31:14 +0530

    New Style configuration is added

---
+++ configuration/index.js
@@ -0,0 +1,14 @@
+let app_config = {
+    hostname: "localhost",
+    app_secret: 'Algo-Network',
+    port: process.env.PORT || 3001,
+    base_url: "http://44.206.245.7",
+    api_version: 'v1'
+};
+
+let db_config = {
+    server_one: "mongodb+srv://MainClusture:94QikJPKmTndGaE3@mainclusture.l3f41ws.mongodb.net/?retryWrites=true&w=majority"
+}
+
+
+module.exports = {app_config, db_config};
--- index.js
+++ index.js
@@ -6,54 +6,56 @@ const cors = require('cors');
 const { ServerApiVersion } = require('mongodb');
 const fileUpload = require('express-fileupload')
 const path = require('path')
-
-let serverApp =  express();
-serverApp.set('hostname', 'localhost');
-        serverApp.set('port', process.env.PORT || 3001);
-
-        serverApp.use(fileUpload())
-
-        // CORS
-        serverApp.options('*', cors());
-        serverApp.use(function (req, res, next) {
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-            res.header("Access-Control-Allow-Headers", "Origin, x-access-token, Content-Type, Accept");
-            next();
-        });
-
-        // Middleware
-
-        serverApp.use(bodyParser.json({limit: '50mb', extended: true}));
-        serverApp.use(bodyParser.urlencoded({extended: false}));
+const Configuration = require('./configuration')
 
 
+let serverApp = express();
+serverApp.set('hostname', Configuration.app_config.hostname);
+serverApp.set('port', Configuration.app_config.port);
+
+serverApp.use(fileUpload())
+
+// CORS
+serverApp.options('*', cors());
+serverApp.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, x-access-token, Content-Type, Accept");
+    next();
+});
+
+// Middleware
+
+serverApp.use(bodyParser.json({ limit: '50mb', extended: true }));
+serverApp.use(bodyParser.urlencoded({ extended: false }));
 
 
-        mongoose.connect('mongodb+srv://MainClusture:94QikJPKmTndGaE3@mainclusture.l3f41ws.mongodb.net/?retryWrites=true&w=majority', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            serverApi:ServerApiVersion.v1,
-            dbName:'Content-Database' ,
-            autoIndex: true,
-        })
-            .then((res) => {console.log('#####---> Mongo DB Connected!');})
-            .catch(err => {console.log("####----> Mongo Db not Connected" + err);});
-
-        serverApp.use('/images', express.static(path.join(__dirname, '../images')));
-
-    
-        route.init(serverApp)
-
-      
 
 
-        // starting server
-        serverApp.listen(serverApp.get("port"), () => {
-            console.log(
-                "Express Server is listening on - https://" + serverApp.get("hostname") + ":" + serverApp.get("port")
-            );
-        });
+mongoose.connect('mongodb+srv://MainClusture:94QikJPKmTndGaE3@mainclusture.l3f41ws.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+    dbName: 'Content-Database',
+    autoIndex: true,
+})
+    .then((res) => { console.log('#####---> Mongo DB Connected!'); })
+    .catch(err => { console.log("####----> Mongo Db not Connected" + err); });
+
+serverApp.use('/images', express.static(path.join(__dirname, '../images')));
+
+
+route.init(serverApp)
+
+
+
+
+// starting server
+serverApp.listen(serverApp.get("port"), () => {
+    console.log(
+        "Express Server is listening on - https://" + serverApp.get("hostname") + ":" + serverApp.get("port")
+    );
+});
 
 
 
